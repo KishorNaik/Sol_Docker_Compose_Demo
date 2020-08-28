e2f282b8fd2861f910bf32e9ae0237cfe0a422f1
Kishor Naik <kishor.naik011.net@outlook.com>
2020-08-28 17:53:49 +0530

    Code Commit

---
+++ Sol_Demo/WebMVC/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://WebApi/WeatherForecast");
+
+                var response = await client.SendAsync(request);
+                ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+            }
+
+            return View();
+        }
+    }
+}
